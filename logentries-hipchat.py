f6f03d8d782b381afdc6366319f9fd5600f6f416
Giovanni Bajo <rasky@develer.com>
2013-03-11 12:51:00 +0100

    Allow to specify host in env

--- logentries-hipchat.py
+++ logentries-hipchat.py
@@ -37,4 +37,4 @@ def logentries():
     return ""
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", "5000")))
+    app.run(host=os.environ.get("HOST", "127.0.0.1"), port=int(os.environ.get("PORT", "5000")))
